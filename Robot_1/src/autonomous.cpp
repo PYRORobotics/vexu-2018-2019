38cccd1d316619fdf80a717ff190c97494ee4571
Brandon Rice <bjrice13@gmail.com>
2018-10-23 13:21:43 -0700

    Fix includes for main.h

--- Robot_1/src/autonomous.cpp
+++ Robot_1/src/autonomous.cpp
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "../include/main.h"
 
 /**
  * Runs the user autonomous code. This function will be started in its own task
@@ -11,4 +11,12 @@
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous()
+{
+
+}
+
+void PYROAutonomous1()
+{
+  
+}
